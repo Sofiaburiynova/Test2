c309bd00ca88ac354e7843f62aa7581592f3f22f
Sofiaburiynova <167516259+Sofiaburiynova@users.noreply.github.com>
2024-05-20 10:32:34 +0300

    Update Mbenchmark.java

--- ttz2/src/main/java/Benchmarks/Mbenchmark.java
+++ ttz2/src/main/java/Benchmarks/Mbenchmark.java
@@ -1,5 +1,4 @@
 package Benchmarks;
-
 import org.example.Main;
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.results.RunResult;
@@ -12,88 +11,8 @@ import java.util.Collection;
 import java.util.Random;
 
 public class Mbenchmark {
-    @Benchmark
-    public void test_sum10() {
-        ArrayList<Double> arr = new ArrayList<Double>();
-        for (int i = 0; i < 100; i++) {
-            Random random = new Random();
-            double randomNumber = random.nextDouble() % 41 - 20;
-            arr.add(randomNumber);
-        }
-        Main._sum(arr);
-    }
-    @Benchmark
-    public void test_sum100() {
-        ArrayList<Double> arr = new ArrayList<Double>();
-        for (int i = 0; i < 1000; i++) {
-            Random random = new Random();
-            double randomNumber = random.nextDouble() % 41 - 20;
-            arr.add(randomNumber);
-        }
-        Main._sum(arr);
-    }
-    @Benchmark
-    public void test_min10(){
-        ArrayList<Double> arr = new ArrayList<Double>();
-        for(int i = 0; i < 100; i++) {
-            Random random = new Random();
-            Double randomNumber = random.nextDouble() % 41 - 20;
-            arr.add(randomNumber);
-        }
-        Main._min(arr);
-    }
-    @Benchmark
-    public void test_min100(){
-        ArrayList<Double> arr = new ArrayList<Double>();
-        for(int i = 0; i < 1000; i++) {
-            Random random = new Random();
-            Double randomNumber = random.nextDouble() % 41 - 20;
-            arr.add(randomNumber);
-        }
-        Main._min(arr);
-    }
-    @Benchmark
-    public void test_max10(){
-        ArrayList<Double> arr = new ArrayList<Double>();
-        for(int i = 0; i < 100; i++) {
-            Random random = new Random();
-            Double randomNumber = random.nextDouble() % 41 - 20;
-            arr.add(randomNumber);
-        }
-        Main._max(arr);
-    }
-    @Benchmark
-    public void test_max100(){
-        ArrayList<Double> arr = new ArrayList<Double>();
-        for(int i = 0; i < 1000; i++) {
-            Random random = new Random();
-            Double randomNumber = random.nextDouble() % 41 - 20;
-            arr.add(randomNumber);
-        }
-        Main._max(arr);
-    }
-    @Benchmark
-    public void test_mult10(){
-        ArrayList<Double> arr = new ArrayList<Double>();
-        for(int i = 0; i < 100; i++) {
-            Random random = new Random();
-            Double randomNumber = random.nextDouble() % 41 - 20;
-            arr.add(randomNumber);
-        }
-        Main._sum(arr);
-    }
-    @Benchmark
-    public void test_mult100(){
-        ArrayList<Double> arr = new ArrayList<Double>();
-        for(int i = 0; i < 1000; i++) {
-            Random random = new Random();
-            Double randomNumber = random.nextDouble() % 21 - 10;
-            arr.add(randomNumber);
-        }
-        Main._sum(arr);
-    }
     public static void main(String[] args) {
-        Options opt = new OptionsBuilder()
+        Options option = new OptionsBuilder()
                 .include(org.example.Main.class.getSimpleName())
                 .warmupTime(TimeValue.seconds(2))
                 .measurementTime(TimeValue.seconds(2))
@@ -103,8 +22,89 @@ public class Mbenchmark {
                 .build();
 
         try {
-            Runner runner = new Runner(opt);
+            Runner runner = new Runner(option);
             Collection<RunResult> result =  runner.run();
         } catch (Exception e) {}
     }
+    @Benchmark
+    public void test_sum_1() {
+        ArrayList<Double> arr = new ArrayList<Double>();
+        for (int i = 0; i < 100; i++) {
+            Random random = new Random();
+            double randomNumber = random.nextDouble() % 41 - 20;
+            arr.add(randomNumber);
+        }
+        Main._sum(arr);
+    }
+    @Benchmark
+    public void test_sum_2() {
+        ArrayList<Double> arr = new ArrayList<Double>();
+        for (int i = 0; i < 1000; i++) {
+            Random random = new Random();
+            double randomNumber = random.nextDouble() % 41 - 20;
+            arr.add(randomNumber);
+        }
+        Main._sum(arr);
+    }
+    @Benchmark
+    public void test_min_3(){
+        ArrayList<Double> arr = new ArrayList<Double>();
+        for(int i = 0; i < 100; i++) {
+            Random random = new Random();
+            Double randomNumber = random.nextDouble() % 41 - 20;
+            arr.add(randomNumber);
+        }
+        Main._min(arr);
+    }
+    @Benchmark
+    public void test_min_4(){
+        ArrayList<Double> arr = new ArrayList<Double>();
+        for(int i = 0; i < 1000; i++) {
+            Random random = new Random();
+            Double randomNumber = random.nextDouble() % 41 - 20;
+            arr.add(randomNumber);
+        }
+        Main._min(arr);
+    }
+    @Benchmark
+    public void test_max_5(){
+        ArrayList<Double> arr = new ArrayList<Double>();
+        for(int i = 0; i < 100; i++) {
+            Random random = new Random();
+            Double randomNumber = random.nextDouble() % 41 - 20;
+            arr.add(randomNumber);
+        }
+        Main._max(arr);
+    }
+    @Benchmark
+    public void test_max_6(){
+        ArrayList<Double> arr = new ArrayList<Double>();
+        for(int i = 0; i < 1000; i++) {
+            Random random = new Random();
+            Double randomNumber = random.nextDouble() % 41 - 20;
+            arr.add(randomNumber);
+        }
+        Main._max(arr);
+    }
+    @Benchmark
+    public void test_mult_7(){
+        ArrayList<Double> arr = new ArrayList<Double>();
+        for(int i = 0; i < 100; i++) {
+            Random random = new Random();
+            Double randomNumber = random.nextDouble() % 41 - 20;
+            arr.add(randomNumber);
+        }
+        Main._sum(arr);
+    }
+    @Benchmark
+    public void test_mult_8(){
+        ArrayList<Double> arr = new ArrayList<Double>();
+        for(int i = 0; i < 1000; i++) {
+            Random random = new Random();
+            Double randomNumber = random.nextDouble() % 21 - 10;
+            arr.add(randomNumber);
+        }
+        Main._sum(arr);
+    }
+
 }
